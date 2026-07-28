615449d84863a2fd24ec4fd0dd151922a79683a0
Abdallah Elamin <Baatook567@gmail.com>
2026-07-29 00:50:58 +0400

    Implement logic to find the largest expense in the findLargestExpense function

--- expense-summary.js
+++ expense-summary.js
@@ -28,11 +28,21 @@ function calculateCategoryTotal(expenses=[], category='') { // should return the
 }
 
 function findLargestExpense(expenses=[]) { // should return the full expense object with the largest amount.
+    let largest = { id: 0, category: '', amount: 0};
 
+    function largestHelperFunc (value) {
+        if (largest.amount < value.amount) {
+            largest = value;
+        }
+    }
+
+    expenses.forEach(largestHelperFunc);
+
+    return largest;
 }
 
 function createExpenseSummary(expenses=[]) { // should return total, foodTotal, transportTotal, and largestExpense.
-
+    
 }
 
 // TEST
@@ -44,7 +54,7 @@ const expenses = [
   { id: 4, category: 'books', amount: 40 },
 ];
 
-console.log(calculateCategoryTotal(expenses, 'food'));
+console.log(findLargestExpense(expenses));
 // console.log(createExpenseSummary(expenses));
 // console.log(calculateCategoryTotal(expenses, 'food'));
 // console.log(calculateCategoryTotal(expenses, 'health'));
